ec53d6e53df3134dea066cb1f212f3d717c8f1e8
Nikki Thomas <thom.nikki@gmail.com>
2014-12-20 20:56:35 -0500

    strings

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,29 @@
 module.exports.formLetter = function(firstName, senderName, message) {
 
 };
+formLetter("Kevin","Cole","Everything is wonderful!");
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
 
 };
 
+sliceItAndCombineIt("Thank goodness it is Saturday",0,6,7,16);
+sliceItAndCombineIt("Thank goodness it is Saturday",1,16,2,5);
+
 module.exports.findFirstMatch = function(text, searchString) {
 
 };
+
+findFirstMatch("Today is Saturday","day");
 
 module.exports.findLastMatch = function(text, searchString) {
 
 };
 
+findLastMatch("Today is Saturday","day");
+
 module.exports.substringBetweenMatches = function(text, searchString) {
 
 };
+
+substringBetweenMatches("Today is Saturday, and tomorrow is Sunday","day");

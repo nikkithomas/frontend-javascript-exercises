c19fe8bf9f7cccaddce74dd05fc19ae2890dbdb6
Nikki Thomas <thom.nikki@gmail.com>
2014-12-21 19:41:09 -0500

    booleans

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -2,18 +2,36 @@ module.exports.equalStrings = function(stringOne, stringTwo) {
 
 };
 
+equalStrings('Florida','Florida')
+equalStrings('Florida','Maryland')
+
 module.exports.notEqual = function(one, two) {
 
 };
+
+notEqual(2,'2')
+notEqual(2,2)
 
 module.exports.inBetween = function(lower, middle, upper) {
 
 };
 
+inBetween(2,7,9)
+inBetween(2,9,7)
+
+
+
 module.exports.outsideRanges = function(number) {
 
 };
 
+outsideRanges(35)
+outsideRanges(79)
+
 module.exports.nameAndPrice = function(name, price) {
 
 };
+
+nameAndPrice('LATimes',2)
+nameAndPrice('NYTimes',0)
+nameAndPrice('LATimes'0)

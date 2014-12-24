5b68ba72c46ace198bde69becc5f503d2d74ab89
Nikki Thomas <thom.nikki@gmail.com>
2014-12-24 13:29:56 -0500

    functions

--- 06-functions/00-basics/challenge.js
+++ 06-functions/00-basics/challenge.js
@@ -1,3 +1,12 @@
-module.exports.helloWorld = undefined;
+module.exports.helloWorld = function(){
+  return 'Hello World';
+};
 
-module.exports.multiply = undefined;
+helloWorld();
+
+module.exports.multiply = function(x,y){
+  return x*y;
+};
+
+multiply(4,7);
+multiply(5,12);

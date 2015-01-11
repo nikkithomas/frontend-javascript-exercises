db3bec3a9bf9b7ff2873383a698b9e20a02a9271
Nikki Thomas <thom.nikki@gmail.com>
2015-01-11 01:30:15 -0500

    if else

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -7,13 +7,18 @@ module.exports.favoriteNumber = function(fav,guess){
 
   }
 
-  else (guess===fav){
+  else{
     return"You got it!";
   }
 };
 
 module.exports.checkLock = function(num1, num2, num3, num4){
-  if (num1===3)||(num1===5)||(num1===7)&&(num2===2)&&(num3>=5&&<=100)&&(num4<9|>20){
+  
+
+  if ((num1===3||num1===5||num1===7)&&
+    (num2===2)&&
+    (num3>=5&&num3<=100)&&
+    (num4<9||num4>20)){
     return "correct";
   }
   else {
@@ -22,25 +27,26 @@ module.exports.checkLock = function(num1, num2, num3, num4){
 };
 
 module.exports.canIGet = function(item,money){
-  if (item=="MacBook Air")&&(money>=999){
-    return "true";
-  }
-  else if (item=="MacBook Pro")&&(money>=1299) {
-    return "true";
-
+  if ((item=='MacBook Air'||item=='Apple Sticker')&&(money>=999&&money<=1299)){
+    return true;
 
   }
 
-  else if (item=="Mac Pro")&&(money>=2499){
-    return "true";
+  else if ((item=='MacBook Pro'||item=='MacBook Air'||item=='Apple Sticker')&&(money>=1299&&money<2499)){
+    return true;
+  }
+
+  else if ((item=='Mac Pro')&&(money>=2499)){
+    return true;
 
   }
 
-  else if (item=="Apple Sticker")&&(money>=1){
-    return "true";
+  else if ((item=='Apple Sticker')&&(money>=1&&money<999)){
+    return true;
+
   }
 
-  else {
-    return "false";
+  else{
+    return false;
   }
 };

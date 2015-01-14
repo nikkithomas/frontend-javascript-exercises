3d4fa078884afab6f904e50ff24abb61b1b5446e
Nikki Thomas <thom.nikki@gmail.com>
2015-01-13 21:29:58 -0500

    objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,40 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle,courseDuration,courseStudents){
+  var course={
+    title: courseTitle,
+    duration: courseDuration,
+    students: courseStudents
+  };
 
-module.exports.addProperty = undefined;
+  return course;
+};
 
-module.exports.formLetter = undefined;
+module.exports.addProperty = function(object,newProp,newValue) {
+  if (! object.hasOwnProperty(newProp)){
+    object[newProp] = newValue;
+  }
+  
+  return object;
+};
 
-module.exports.canIGet = undefined;
+
+module.exports.formLetter = function(letter){
+//   var letter={
+//     recipient:"David",
+//     sender:"Arnold",
+//     msg:"What up, thug?"
+// }
+
+return "Hello "+letter.recipient+",\n\n"+letter.msg+"\n\nSincerely,\n"+letter.sender;
+};
+
+module.exports.canIGet = function(item,money){
+  var priceTable={
+    'MacBook Air': 999,
+    'MacBook Pro': 1299,
+    'Mac Pro': 2499,
+    'Apple Sticker': 1
+  };
+  
+  return priceTable[item] <= money;
+
+};
